0f2288b37718f32267fbc86632a7c050efbaf5df
Colby Betterton <colbybetterton@gmail.com>
2023-08-21 18:42:30 -0400

    Added searching functionality + user input

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Web Scraper
 
 This program is intended to scrape the prices of a given search result off Amazon. Currently in the process of being built.
+
+NOTE: You need a chrome driver compatible with your version of chrome in your own project.
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+def launchBrowser():
+    #Get user input
+    search = input("What do you want to search for?")
+    
+    #Init. driver + go to amazon
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(2)
+    driver.get("http://www.amazon.com/")
+
+    #Get search bar element and search for user's input
+    search_bar = driver.find_element(By.ID, 'twotabsearchtextbox')
+    search_bar.send_keys(search)
+    search_bar.submit()
+
+    while(True):
+        pass
+   
+launchBrowser()
